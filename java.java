72a9594721381f9864c9ddba06b121c3e4b16352
vidhyashreeS <74619360+vidhyashreeS@users.noreply.github.com>
2024-02-14 15:10:00 +0530

    Update java.java

--- java.java
+++ java.java
@@ -1,11 +1,5 @@
-public class IceCream {
-
-	int price;
-	String brand;
-	String flavour;
-	String type;
-	
-	void printDetails(){
-		System.out.println("price: "+price+"\n"+"brand:"+brand+ "\n"+ "flavour: "+flavour+ "\n"+ "type: "+type);
+class HelloWorld {
+	public static void main(String []args) {
+		System.out.println("hello");
 	}
 }
