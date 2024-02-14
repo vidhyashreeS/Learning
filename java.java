0c6a08f4047715978fa13913279fc475453de69d
vidhyashreeS <74619360+vidhyashreeS@users.noreply.github.com>
2024-02-14 14:53:34 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,11 @@
+public class IceCream {
+
+	int price;
+	String brand;
+	String flavour;
+	String type;
+	
+	void printDetails(){
+		System.out.println("price: "+price+"\n"+"brand:"+brand+ "\n"+ "flavour: "+flavour+ "\n"+ "type: "+type);
+	}
+}
