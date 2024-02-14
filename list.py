a3e6f18057910e7672f86ff7924d07185e9ac8e5
vidhyashreeS <74619360+vidhyashreeS@users.noreply.github.com>
2024-02-14 12:58:22 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,15 @@
+import os
+List1 = [8, 9, 12, 2, 22, 16]
+List1.reverse()
+print("The reversed list is :", List1)
+
+List2 = [91, 67, 120, 54, 38, 110, 2]
+List2.sort()
+print("The sorted list is : ", List2)
+
+List3 = []
+List3 = List1.copy()
+print("List3 =", List3)
+
+
+
